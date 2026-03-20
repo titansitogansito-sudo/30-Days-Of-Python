88c61fa0e4f461de4fc17f6e8c31356c48686c37
titansitogansito-sudo <titansitogansito@gmail.com>
2026-03-20 17:48:24 -0400

    Add variables for personal information and details

--- 02_Day_Variables_builtin_functions/variables2.py
+++ 02_Day_Variables_builtin_functions/variables2.py
@@ -1,1 +1,12 @@
+# Day 2: 30 Days of python programming 
+
+first_name = "ALEXANDER"
+last_name = "josé"
+full name = "Alexander valentin josé Bermudez"
+country = "USA"
+city = "new york"
+age = 24
+year = 2026
+is_married = true
+dog_name, selected_item, food = "golfo", "mouse", "pizza"
 
